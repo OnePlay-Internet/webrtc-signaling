61049dc4513b4430c3775fd4375965fc25bbdcb5
pigeatgarlic <huyhoangdo0205@gmail.com>
2022-09-18 12:16:38 +0700

    enable argument

--- main.go
+++ main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/pigeatgarlic/signaling/protocol"
 	signalling "github.com/pigeatgarlic/signaling/signaling"
 )
 
 func main() {
-	shutdown := make(chan bool)
+	ValidationUrl := "https://auth.thinkmay.net/auth/validate";
+	WebsocketPort := 8088;
+	GrpcPort :=      8000;
+
+	var err error
+	args := os.Args[1:]
+	for i, arg := range args {
+		if arg == "--websocket" {
+			WebsocketPort,err = strconv.Atoi(args[i+1])
+		} else if arg == "--grpc" {
+			GrpcPort,err = strconv.Atoi(args[i+1])
+		} else if arg == "--validationurl" {
+			ValidationUrl = args[i+1]
+		} else if arg == "--help" {
+			fmt.Printf("--engine |  encode engine ()\n")
+			return
+		}
+	}
+
+	if err != nil  {
+		fmt.Printf("faile to parse argument: %s\n",err.Error());
+		return;
+	}
+
 	signalling.InitSignallingServer(&protocol.SignalingConfig{
-		WebsocketPort: 8088,
-		GrpcPort:      8000,
-		ValidationUrl: "https://auth.thinkmay.net/auth/validate",
+		WebsocketPort: WebsocketPort,
+		GrpcPort:      GrpcPort,
+		ValidationUrl: ValidationUrl,
 	})
+
+	shutdown := make(chan bool)
 	shutdown <- true
 }
